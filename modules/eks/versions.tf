7d8685301c229f3e6066baae9d412ae4be16abe9
malibalakrishna <143379461+malibalakrishna@users.noreply.github.com>
2025-09-21 22:31:33 +0530

    Update versions.tf

--- modules/eks/versions.tf
+++ modules/eks/versions.tf
@@ -1,1 +1,17 @@
-
+terraform {
+required_version = ">= 1.5"
+required_providers {
+aws = {
+source = "hashicorp/aws"
+version = ">= 5.50"
+}
+helm = {
+source = "hashicorp/helm"
+version = ">= 2.13"
+}
+kubernetes = {
+source = "hashicorp/kubernetes"
+version = ">= 2.29"
+}
+}
+}
