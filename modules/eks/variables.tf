586b1441f7266d7de7292244a7c4e3a8708f430f
malibalakrishna <143379461+malibalakrishna@users.noreply.github.com>
2025-09-21 22:32:03 +0530

    Update variables.tf

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -1,64 +1,34 @@
-variable "cluster_name" {
-  description = "Name of the EKS cluster"
-  type        = string
+variable "cluster_name" { type = string }
+variable "cluster_version" { type = string # e.g., "1.30"
+validation {
+condition = can(regex("^1\\.(2[0-9]|3[0-1])$", var.cluster_version))
+error_message = "Provide a supported EKS version string, e.g. 1.29 or 1.30."
+}
 }
 
-variable "cluster_version" {
-  description = "Kubernetes minor version to use for the EKS cluster"
-  type        = string
-  default     = null
+
+variable "vpc_id" { type = string }
+variable "private_subnet_ids" { type = list(string) }
+
+
+variable "instance_types" {
+type = list(string)
+default = ["t3.large"]
 }
 
-variable "vpc_id" {
-  description = "ID of the VPC where the cluster and its nodes will be provisioned"
-  type        = string
+
+variable "capacity_type" {
+type = string
+default = "ON_DEMAND" # or "SPOT"
 }
 
-variable "subnet_ids" {
-  description = "List of subnet IDs where the cluster control plane will be provisioned"
-  type        = list(string)
-}
-
-variable "control_plane_subnet_ids" {
-  description = "List of subnet IDs where the cluster control plane will be provisioned"
-  type        = list(string)
-}
-
-variable "cluster_endpoint_private" {
-  description = "Enable private API server endpoint"
-  type        = bool
-  default     = false
-}
-
-variable "cluster_endpoint_public" {
-  description = "Enable public API server endpoint"
-  type        = bool
-  default     = true
-}
-
-variable "cluster_endpoint_public_access_cidrs" {
-  description = "List of CIDR blocks that can access the Amazon EKS public API server endpoint"
-  type        = list(string)
-  default     = ["0.0.0.0/0"]
-}
-
-variable "cluster_enabled_log_types" {
-  description = "A list of the desired control plane logs to enable"
-  type        = list(string)
-  default     = ["audit", "api", "authenticator", "controllerManager", "scheduler"]
-}
-
-variable "cluster_addons" {
-  description = "Map of cluster addon configurations"
-  type = map(object({
-    version = string
-    resolve_conflicts = string
-  }))
-  default = {}
-}
 
 variable "tags" {
-  description = "A map of tags to assign to the resource"
-  type        = map(string)
-  default     = {}
+type = map(string)
+default = {}
 }
+
+
+# Cluster Autoscaler settings
+variable "enable_cluster_autoscaler" { type = bool, default = true }
+variable "cluster_autoscaler_chart_version" { type = string, default = "9.44.0" }
